c96ec84d5553dbd409fce71303bc51d0606f2cf7
Daniel Catori <caaatori@gmail.com>
2024-06-29 21:57:13 -0300

    DepartmentDao implementation and test

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.mysql.cj.xdevapi.PreparableStatement;
-
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
